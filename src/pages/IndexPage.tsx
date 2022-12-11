e32b9888ef5ae576e058bde69335f4695c04d60f
Filip Velebit Samodol <filip.samodol@coreline.agency>
2022-12-11 12:00:38 +0100

    Finish layout for the form

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -7,13 +7,17 @@ import { PageContainer } from "./components/PageContainer";
 import { Txt } from "./components/Txt";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "./components/Button";
 
 /** @jsxImportSource @emotion/react */
 
 export const IndexPage = () => {
   const schema = yup.object().shape({
     opinion: yup.string().required(),
-    rating: yup.number().required(),
+    rating: yup
+      .object()
+      .shape({ value: yup.number(), label: yup.string() })
+      .nullable(),
   });
 
   const methods = useForm({
@@ -25,15 +29,39 @@ export const IndexPage = () => {
     mode: "onSubmit",
   });
 
+  const onSubmit = methods.handleSubmit(async (values) => {
+    try {
+      console.log(values);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
   return (
     <PageContainer
       containerCss={[tw`h-screen flex items-center justify-center`]}
     >
-      <div tw="w-1/4 h-1/4 bg-gray-300 p-6 flex flex-col justify-between">
+      <div tw="bg-gray-300 p-12 flex flex-col justify-between h-2/5 w-3/5">
         <Txt>Rate this movie!</Txt>
         <FormProvider {...methods}>
-          <SimpleInputField name={"opinion"} />
-          <TextSectionDropdownInput name={"rating"} options={[]} />
+          <SimpleInputField
+            name={"opinion"}
+            placeholder="Let us know what you tought about the movie!"
+            inputCss={[tw`text-sm`]}
+          />
+          <TextSectionDropdownInput
+            name={"rating"}
+            options={[{ value: 1, label: "1" }]}
+            placeholder="Your rating"
+            inputCss={[tw`text-white text-sm`]}
+          />
+
+          <Button
+            text={"Submit"}
+            containerCss={[tw`border-2 border-r-2 border-white py-2 px-2`]}
+            inputCss={[tw`text-gray-900 font-semibold text-2xl`]}
+            onClick={onSubmit}
+          />
         </FormProvider>
       </div>
     </PageContainer>
