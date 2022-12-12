794af76ee1b17b8cea1d5a73674b2c28b7d03de4
Filip Velebit Samodol <filip.samodol@coreline.agency>
2022-12-12 17:45:11 +0100

    Fix backend client

--- src/api/axiosServices/axiosBackendClient.ts
+++ src/api/axiosServices/axiosBackendClient.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosBackendClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001",
+  baseURL:
+    process.env.REACT_APP_API_URL + "/api/v1" || "http://localhost:3001/api/v1",
 });
